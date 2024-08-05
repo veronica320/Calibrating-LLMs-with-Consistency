340f647867d3a3c6136c127663513c211545b80e
veronica320 <lyuqing@sas.upenn.edu>
2024-08-05 00:14:16 -0400

    olmo it & it-rl calibration across format files

--- source/visualization/gen_compare_output_formats_figure.py
+++ source/visualization/gen_compare_output_formats_figure.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
 	parser.add_argument("--dataset_names", nargs="+", default=["ASDIV", "CLUTRR", "GSM8K", "DATE", "MULTIARITH", "SAYCAN", "SPORT", "STRATEGYQA", "SVAMP"])
 	parser.add_argument("--LMs", nargs="+", default=["code002", "gpt-3.5-turbo", "gpt4", "llama-7B", "llama-13B", "llama-70B", "mistral-7B", "mistral-7B-it", "olmo-7B", "olmo-7B-it", "olmo-7B-it-rl"])
 	parser.add_argument("--output_formats", nargs="+", default=["standard", "COT", "LtM", "noNL", "NL+SL"])
-	parser.add_argument("--split", type=str, default="test", choices=["test", "dev_100"])
+	parser.add_argument("--split", type=str, default="test", choices=["test"])
 	parser.add_argument("--n_vote", type=int, default=40)
 	parser.add_argument("--consistency_metrics", nargs="+", default=["entropy", "agree_percent", "fsd"])
 	parser.add_argument("--n_bins", type=int, default=10)
